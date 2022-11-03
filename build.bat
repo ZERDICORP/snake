11ade8fe17c2b65c080d71cf12db114f292640a5
ZERDICORP <nikolaianikin2002@gmail.com>
2022-11-03 15:28:47 +0300

    refactored

--- build.bat
+++
@@ -1,3 +0,0 @@
-g++ ./src/main.cpp ./src/implementation/*.cpp -DSFML_STATIC -I"C:/Program Files/SFML-2.5.1/include" -I"./src/include" -I"./src/headers" -o ./src/build/main -L"C:/Program Files/SFML-2.5.1/lib" -lsfml-graphics -lsfml-window -lsfml-system -mwindows
-start ./src/build/main.exe
-pause
